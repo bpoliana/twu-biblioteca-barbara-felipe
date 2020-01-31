49d36c3c0ba9dcd67c606f2094db890496f6e0be
Barbara Jaber <barbarajaber@Barbaras-MacBook-Pro.local>
2020-01-31 00:21:59 -0300

    Add BookList class and tests

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -4,7 +4,10 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
         Message welcomeMessage = new Message();
-
         System.out.println(welcomeMessage.welcomeMessage());
+
+        BookList list = new BookList();
+        System.out.println("Books:");
+        System.out.println(list.getBookList().toString());
     }
 }
